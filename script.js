2eeae97d75543572a9415051e83ba34b72bfe999
jpcamba <camba.jannieca@gmail.com>
2019-06-12 23:34:06 +0800

    Added carousel popup when section is clicked (for one section only). Next: Add legit views and enable clicking of all sections

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
+
+
 function func(element) {
-    console.log(element);
-    alert('sdfds');
+    $('#seat-view').css('display', 'block');
 }
+
+$(document).click(function(event) {
+    console.log(event);
+    if (event.target.id === 'seat-view') {
+        $('#seat-view').css('display', 'none');
+    }
+});
