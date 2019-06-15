7899b4cf9792822ca627c849a3d1bad711167c98
jpcamba <camba.jannieca@gmail.com>
2019-06-15 13:48:02 +0800

    Adding all available views

--- data.js
+++ data.js
@@ -2,5 +2,68 @@ var SEATVIEW_MAP = {
     vipAT: [
         { filename: '1.jpg', source: 'https://twitter.com/alyssachoi407' },
         { filename: '2.jpg', source: 'https://twitter.com/alyssachoi407' },
+        { filename: '3.jpg', source: 'https://facebook.com/groups/cassph/' }
+    ],
+    vipA202: [
+        { filename: '1.jpg', source: 'https://twitter.com/happeehour' }
+    ],
+    vipA204: [
+        { filename: '1.jpg', source: 'https://facebook.com/groups/cassph/' }
+    ],
+    vipA205: [
+        { filename: '1.jpg', source: 'https://facebook.com/groups/cassph/' },
+        { filename: '2.jpg', source: 'https://facebook.com/groups/cassph/' }
+    ],
+    vipA216: [
+        { filename: '1.jpg', source: 'https://twitter.com/tvxqinmanila/' }
+    ],
+    vipA217: [
+        { filename: '1.jpg', source: 'https://twitter.com/tvxqinmanila/' }
+    ],
+    vipA218: [
+        { filename: '1.jpg', source: 'https://facebook.com/groups/cassph/' }
+    ],
+    vipA220: [
+        { filename: '1.jpg', source: 'https://twitter.com/marzorie25/' }
+    ],
+    vipA221: [
+        { filename: '1.jpg', source: 'https://twitter.com/tvxqinmanila/' }
+    ],
+    vipA222: [
+        { filename: '1.jpg', source: 'https://twitter.com/tvxqinmanila/' },
+        { filename: '2.jpg', source: 'https://twitter.com/tvxqinmanila/' },
+        { filename: '3.jpg', source: 'https://twitter.com/tvxqinmanila/' },
+        { filename: '4.jpg', source: 'https://facebook.com/groups/cassph/' }
+    ],
+    ubCenter401: [
+        { filename: '1.jpg', source: 'https://twitter.com/tvxqinmanila' },
+        { filename: '2.jpg', source: 'https://twitter.com/happeehour' }
+    ],
+    ubCenter402: [
+        { filename: '1.jpg', source: 'https://facebook.com/groups/cassph/' }
+    ],
+    ubCenter422: [
+        { filename: '1.jpg', source: 'https://twitter.com/tvxqinmanila' }
+    ],
+    ubA419: [
+        { filename: '1.jpg', source: 'https://facebook.com/groups/cassph/' }
+    ],
+    ubA420B: [
+        { filename: '1.jpg', source: 'https://twitter.com/tvxqinmanila' },
+        { filename: '2.jpg', source: 'https://twitter.com/tvxqinmanila' }
+    ],
+    gaCenter522: [
+        { filename: '1.jpg', source: 'https://twitter.com/tvxqinmanila' },
+        { filename: '2.jpg', source: 'https://twitter.com/tvxqinmanila' }
+    ],
+    gaA519: [
+        { filename: '1.jpg', source: 'https://twitter.com/tvxqinmanila' }
+    ],
+    gaA520B: [
+        { filename: '1.jpg', source: 'https://twitter.com/tvxqinmanila' },
+        { filename: '2.jpg', source: 'https://facebook.com/groups/cassph/' }
+    ],
+    gaB506: [
+        { filename: '1.jpg', source: 'https://facebook.com/groups/cassph/' }
     ]
 };
--- script.js
+++ script.js
@@ -15,6 +15,12 @@ function displaySeatView(section) {
     $('#seat-view').css('display', 'block');
     
     var images = SEATVIEW_MAP[section]; 
+    if (images === null || images === undefined) {
+        var html = getNotAvailableHtml();
+        $('#carousel-images').html(html);
+        return;
+    }
+
     var html = getImageHtml(getImagePath(section, images[0].filename), images[0].source, true);
     for (var i = 1; i < images.length; i++) {
         html += getImageHtml(getImagePath(section, images[i].filename), images[i].source, false);
@@ -34,5 +40,10 @@ function getImageHtml(link, source, isActive) {
     html += '<span style="color:white;font-family:Verdana;font-size:20px;">Source: ';
     html += '<a href="' + source + '">' + source + '</a></span>';
     html += '</div>';
+    return html;
+}
+
+function getNotAvailableHtml() {
+    var html = '<div style="color:white;font-family:Verdana;font-size:20px;text-align:center;">View not available</div>';
     return html;
 }
